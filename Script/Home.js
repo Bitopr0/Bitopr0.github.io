b3a3718e8f02fd5e4e74b12855d7f60991e3d97f
BitPr0 <miller.alexlove@gmail.com>
2025-09-07 13:09:26 +0800

    1

--- Script/Home.js
+++ Script/Home.js
@@ -1,15 +1,19 @@
 let amountSpan;
 let currentAmount;
+const $id = (id) => document.getElementById(id);
 
 window.onload = function () {
-  amountSpan = document.getElementById("amount");
+  amountSpan = $id("amount");
 
   currentAmount = Math.floor(Math.random() * (90000 - 100 + 1)) + 100;
   updateAmountDisplay(currentAmount);
 
   amountSpan.style.cursor = "pointer";
   amountSpan.addEventListener("click", handleAmountChange);
-  amountSpan.addEventListener("click",)
+
+  $id("toolbar1").addEventListener("click", () => {
+    $id("QRcode").click();
+  });
 };
 
 function updateAmountDisplay(amount) {
@@ -20,7 +24,7 @@ function updateAmountDisplay(amount) {
 function handleAmountChange() {
   const input = prompt("請輸入新的金額：", currentAmount);
   if (input !== null && !isNaN(input)) {
-    currentAmount = parseInt(input);
+    currentAmount = parseInt(input, 10);
     updateAmountDisplay(currentAmount);
   } else if (input !== null) {
     alert("請輸入有效的數字！");
